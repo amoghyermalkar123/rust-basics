cb88a18a3c020b7d298ad15337e3ca0b79d2ad0a
amogh <ayermalkar99@gmail.com>
2021-09-17 15:54:15 +0530

    Sliceees

--- ownership/src/slice.rs
+++ ownership/src/slice.rs
@@ -2,3 +2,25 @@
 // Slices let you reference a contiguous sequence of elements 
 // in a collection rather than the whole collection.
 
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' { // byte literal
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+
+pub fn main_two() {
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", word);
+}
