d870cb2012c29ef97acb76f27f94fafbe7f3086c
amogh <ayermalkar99@gmail.com>
2021-09-19 18:56:53 +0530

    using examples

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 
 fn main() {
-    structs::start()   
+    let ipv4 =  enums::IpAddr::V4(String::from("127.0.0.1"));
+
+    let ipv6 =  enums::IpAddr::V6(String::from("2402:e280:3e10:49:4d8c:40d3:72ec:8e83"));
+
+    enums::sort_addresses(ipv4);
+    enums::sort_addresses(ipv6);
+    enums::test_options();
+
+    error_handling::err_handling()
 }
